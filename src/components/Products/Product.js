cd7505f5dc80d6fa6f00ac3ef349d9be7b9fb8f5
hendlegleg <hendlegleg1@gmail.com>
2024-05-10 11:29:41 +0100

    hendh

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -2,22 +2,18 @@ import React, { useState } from "react";
 export default function Product(props) {
   const [identid, setIdentId] = useState();
   const [disabled, setquantit] = useState(1);
-  const { onidprod, search, onAction } = props;
+  const { search, onAction } = props;
+  const onidprod = (e) => {
+    setIdentId(e.target.value);
+  };
   return (
     <div>
       <div>
         <label>Saisir l'identifiant du produit</label>
-        <input
-          type="text"
-          value={search}
-          onChange={(event) => {
-            const value = event.target.value;
-            onidprod(value);
-          }}
-        ></input>{" "}
+        <input type="text" value={search} onChange={onidprod}></input>{" "}
       </div>
       <div>
-        <button onClick={onAction}>Search</button>{" "}
+        <button onClick={onAction}>Valider</button>{" "}
       </div>
       <div>
         <label>Saisir la quantité</label>
